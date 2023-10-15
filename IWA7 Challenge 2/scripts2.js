1daa921e53c3c174e5d733209e8432ec5decdfe5
Denelle97 <denelle728@gmail.com>
2023-10-15 16:58:47 +0200

    changes to C2. coach validated C2

--- IWA7 Challenge 2/scripts2.js
+++ IWA7 Challenge 2/scripts2.js
@@ -1,10 +1,4 @@
 const nickname= "Timmy";
 const firstname = "Timothy";
 
-if(nickname){
-    console.log(`Good Morning ${nickname}!`)
-} else if(firstname) {
-    console.log(`Good Morning ${firstname}!`)
-}else{
-    console.log(`Good Morning!`)
-}
+console.log(`Good Morning ${nickname ? nickname : firstname ? firstname : ''}!`);
