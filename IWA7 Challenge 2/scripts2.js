887cce8b8969e97e8ec3f8be67ccfe65fb06ae66
Denelle97 <denelle728@gmail.com>
2023-10-15 18:40:57 +0200

    completed C3

--- IWA7 Challenge 2/scripts2.js
+++ IWA7 Challenge 2/scripts2.js
@@ -2,3 +2,5 @@ const nickname= "Timmy";
 const firstname = "Timothy";
 
 console.log(`Good Morning ${nickname ? nickname : firstname ? firstname : ''}!`);
+
+//Question the way Coach or other students did this challenge.
