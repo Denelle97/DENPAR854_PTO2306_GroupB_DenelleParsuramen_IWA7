1a7dc52ff49485d0921be2c0f29fb928e1884770
Denelle97 <denelle728@gmail.com>
2023-10-15 11:07:38 +0200

    completed challenge 2

--- IWA7 Challenge 2/scripts2.js
+++ IWA7 Challenge 2/scripts2.js
@@ -1,4 +1,13 @@
 const nickname= "Timmy";
 const firstname = "Timothy";
 
-console.log("Good Morning, ${nickname} || {firstname}!") //us if 
+
+if (nickname) {
+    console.log("Good Morning, ${nickname}!");
+} else if (firstname) {
+    console.log("Good Morning, ${firstname}!");
+} else {
+    console.log("Good Morning!");
+} 
+
+
